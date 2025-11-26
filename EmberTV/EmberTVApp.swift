b8ad46c2409d544e121b516905b711f703ba540f
Brandon Duncan <brandon@regentmediagroup.com>
2025-11-26 15:05:31 -0500

    Initial Commit

---
+++ EmberTV/EmberTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmberTVApp.swift
+//  EmberTV
+//
+//  Created by Brandon Duncan on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct EmberTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
